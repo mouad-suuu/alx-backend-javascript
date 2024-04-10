48f11c25db3638a0cb825a4d65e23040a2456c37
mouad mennioui <moad.menniui@gmail.com>
2024-04-10 19:42:52 +0000

    Update .eslintrc.js

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,16 +1,28 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
-    "jest/globals": true,
+    browser: false,
+    es6: true,
+    jest: true,
   },
-  extends: ["standard"],
+  extends: ["airbnb-base", "plugin:jest/all"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: "module",
   },
   plugins: ["jest"],
   rules: {
-    // Your custom rules here
+    "no-console": "off",
+    "no-shadow": "off",
+    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
   },
+  overrides: [
+    {
+      files: ["*.js"],
+      excludedFiles: "babel.config.js",
+    },
+  ],
 };
