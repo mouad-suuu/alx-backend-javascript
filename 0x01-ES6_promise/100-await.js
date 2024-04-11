af5c11a5c81e60ad0def4b9f95e9498f374726aa
mouad mennioui <moad.menniui@gmail.com>
2024-04-11 01:38:35 +0000

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,13 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from "./utils.js";
 
-export default async function asyncUploadUser() {
-  let res = {};
-
+async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    res = { photo, user };
-  } catch (err) {
-    res = { photo: null, user: null };
+    return { photo, user };
+  } catch {
+    return { photo: null, user: null };
   }
-  return res;
 }
+
+export default asyncUploadUser;
