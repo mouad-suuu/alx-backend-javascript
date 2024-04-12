6a3569e9d029d37126f4c10d5176b4aa430ab1a8
mouad mennioui <moad.menniui@gmail.com>
2024-04-12 01:48:41 +0000

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,11 +4,29 @@ export default class HolbertonClass {
     this.location = location;
   }
 
-  valueOf() {
+  get size() {
     return this._size;
   }
 
-  toString() {
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
     return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
   }
 }
