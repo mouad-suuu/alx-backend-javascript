d0f73f53e0d9af8717a88b2073b152de7189bbaf
mouad mennioui <moad.menniui@gmail.com>
2024-04-12 01:22:06 +0000

    Create 6-sky_high.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from "./5-building.js";
+
+class SkyHighBuilding {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+  evacuationWarnigMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
