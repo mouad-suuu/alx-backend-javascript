4c8ca33aab290ea820726edfa1c18ddffd0b2ac0
mouad mennioui <moad.menniui@gmail.com>
2024-04-12 01:53:57 +0000

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-// Define the HolbertonClass
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -14,7 +13,6 @@ export class HolbertonClass {
   }
 }
 
-// Define the StudentHolberton class
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -31,26 +29,20 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - \
+${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-// Create instances of HolbertonClass
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-// Create instances of StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Export the list of students
-export const listOfStudents = [
-  student1,
-  student2,
-  student3,
-  student4,
-  student5,
-];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
