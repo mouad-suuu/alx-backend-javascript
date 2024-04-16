9538ca41a513bdcd92108444611272cbd4e332ce
mouad mennioui <moad.menniui@gmail.com>
2024-04-16 19:19:53 +0100

    Create 100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+export const weakMap = new WeakMap();
+
+export default function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    let count = weakMap.get(endpoint);
+    count++;
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, count);
+  }
+}
