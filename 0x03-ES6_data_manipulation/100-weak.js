699cea98117bb8f931ba9a882b52cdf487ec0f38
mouad mennioui <moad.menniui@gmail.com>
2024-04-16 19:21:59 +0100

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,13 @@
 export const weakMap = new WeakMap();
 
-export default function queryAPI(endpoint) {
+const MAX_ENDPOINT_CALLS = 5;
+
+export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else {
-    let count = weakMap.get(endpoint);
-    count++;
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, count);
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
   }
 }
