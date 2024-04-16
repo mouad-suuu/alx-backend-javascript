5deb485af7b6eb823f37d9058edc92866498d2d8
mouad mennioui <moad.menniui@gmail.com>
2024-04-16 17:42:06 +0100

    Create 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstname: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstname: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstname: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+}
