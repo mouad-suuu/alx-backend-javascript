50d8f258c66419c8340ad88958e019187a59bd91
mouad mennioui <moad.menniui@gmail.com>
2024-06-05 00:31:59 +0100

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,23 +1,23 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
   if (!fs.statSync(dataPath).isFile()) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
   const fileLines = fs
-    .readFileSync(dataPath, "utf-8")
-    .toString("utf-8")
+    .readFileSync(dataPath, 'utf-8')
+    .toString('utf-8')
     .trim()
-    .split("\n");
+    .split('\n');
   const studentGroups = {};
-  const dbFieldNames = fileLines[0].split(",");
+  const dbFieldNames = fileLines[0].split(',');
   const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
   for (const line of fileLines.slice(1)) {
-    const studentRecord = line.split(",");
+    const studentRecord = line.split(',');
     const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
     const field = studentRecord[studentRecord.length - 1];
     if (!Object.keys(studentGroups).includes(field)) {
@@ -31,13 +31,13 @@ const countStudents = (dataPath) => {
   }
 
   const totalStudents = Object.values(studentGroups).reduce(
-    (pre, cur) => (pre || []).length + cur.length
+    (pre, cur) => (pre || []).length + cur.length,
   );
   console.log(`Number of students: ${totalStudents}`);
   for (const [field, group] of Object.entries(studentGroups)) {
-    const studentNames = group.map((student) => student.firstname).join(", ");
+    const studentNames = group.map((student) => student.firstname).join(', ');
     console.log(
-      `Number of students in ${field}: ${group.length}. List: ${studentNames}`
+      `Number of students in ${field}: ${group.length}. List: ${studentNames}`,
     );
   }
 };
