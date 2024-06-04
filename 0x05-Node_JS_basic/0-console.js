a6ed1541aac76521e21d86ef8b8843dee2d8a9e8
mouad mennioui <moad.menniui@gmail.com>
2024-06-04 17:30:56 +0100

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,8 @@
 /**In the file 0-console.js, create a function
  * named displayMessage that
  * prints in STDOUT the string argument. */
-const displayMessage = (msg) => {
-  console.log(msg);
+const displayMessage = (string) => {
+  console.log(string);
 };
 
 module.exports = displayMessage;
