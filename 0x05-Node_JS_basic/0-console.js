e983c805fe9a77312af72f52ed9216d5716928a4
mouad mennioui <moad.menniui@gmail.com>
2024-06-04 17:33:46 +0100

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,4 @@
-/**In the file 0-console.js, create a function
+/** In the file 0-console.js, create a function
  * named displayMessage that
  * prints in STDOUT the string argument. */
 const displayMessage = (string) => {
