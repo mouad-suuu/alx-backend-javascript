264ae4449d51416db434463a3125c4aacb43c125
mouad mennioui <moad.menniui@gmail.com>
2024-06-06 22:33:32 +0100

    Create 1-calcul.js

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,17 @@
+const calculateNumber = (type, a, b) => {
+  if (type === "SUM") {
+    return Math.round(a) + Math.round(b);
+  }
+  if (type === "SUBTRACT") {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === "DIVIDE") {
+    if (b === 0) {
+      return "Error";
+    } else if (b !== 0) {
+      return Math.round(a) / Math.round(b);
+    }
+  }
+};
+
+module.exports = calculateNumber;
